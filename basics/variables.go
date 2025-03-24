06b615a6183affe99cc008db4892b985bd0b1af8
deep-go-lang <204604877+deep-go-lang@users.noreply.github.com>
2025-03-25 01:25:06 +0530

    hello to naming conventions

---
+++ basics/variables.go
@@ -0,0 +1,29 @@
+package basics
+
+import "fmt"
+
+var middleName string = "Rohan"
+
+func main() {
+	// var age int
+	// var name string = "Aishwarya"
+	// var name1 = "Tapsee"
+
+	// count := 20
+	// lastname := "Rai"
+
+	// fmt.Println(firstName)
+
+	// middleName := "Veena"
+
+	// middleName := "Rohan"
+
+	fmt.Println(middleName)
+
+}
+
+
+func printName() {
+	firstName := "Neha"
+	fmt.Println(firstName)
+}
